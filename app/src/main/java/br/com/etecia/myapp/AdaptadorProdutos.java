dd20aa088775965921288013826b558fac837e1c
rafalvs <rafaelalvessantana0401@gmail.com>
2025-08-08 22:21:31 -0300

    up

---
+++ app/src/main/java/br/com/etecia/myapp/AdaptadorProdutos.java
@@ -0,0 +1,65 @@
+package br.com.etecia.myapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class AdaptadorProdutos extends RecyclerView.Adapter<AdaptadorProdutos.ViewHolder> {
+    private Context context;
+
+    private List<Produto> lstProdutos;
+
+    public AdaptadorProdutos(Context context, List<Produto> lstProdutos) {
+        this.context = context;
+        this.lstProdutos = lstProdutos;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.modelo_produtos, parent, attachToRoot:false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.modTituloProduto.setText(lstProdutos.get(position).getTitulo());
+        holder.modImagemProduto.setText(lstProdutos.get(position).getTitulo());
+
+        holder.cardProdutos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return lstProdutos.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        CardView cardProdutos;
+        ImageView modImagemProduto;
+
+        TextView modTituloProduto;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            cardProdutos = itemView.findViewById(R.id.cardAnimals);
+            modImagemProduto = itemView.findViewById(R.id.modImagemProduto);
+            modTituloProduto = itemView.findViewById(R.id.modTitlo);
+        }
+    }
+
+}
